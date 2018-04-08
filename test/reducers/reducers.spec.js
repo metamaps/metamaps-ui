2e808d8b92393bba7f8e752c6c127a27d1f801ef
Connor Turland <connorturland@gmail.com>
2018-04-08 16:35:31 -0400

    fix test

--- test/reducers/reducers.spec.js
+++ test/reducers/reducers.spec.js
@@ -15,6 +15,7 @@ describe('reducers.js', function() {
       },
       ui: {
         currentUserId: null,
+        mobile: false,
         mobileTitle: '',
         mobileTitleWidth: 0,
         toast: null,
